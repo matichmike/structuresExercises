28a57f3052598fb93993f646573fff1fed05400d
Michael Matich <matich.mike@gmail.com>
2020-07-11 10:01:16 -0400

    additional algorithm exercises

--- algorithmScripting.js
+++ algorithmScripting.js
@@ -13,3 +13,31 @@ function factorialize(num) {
 }
 factorialize(5);
 
+// Return the length of the longest word in the provided sentence
+function findLongestWordLength(str) {
+  let result = 0;
+  let arr = str.split(" ");
+  for (let ar of arr) {
+    if (ar.length > result) {
+      result = ar.length;
+    }
+  }
+  return result;
+}
+findLongestWordLength("The quick brown fox jumped over the lazy dog");
+
+// Return an array consisting of the largest number from each provided sub-array
+function largestOfFour(arr) {
+  let result = [];
+  for (let ar of arr) {
+    result.push(Math.max(...ar));
+  }
+  return result;
+}
+largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
+
+// Check if a string (first argument, str) ends with the given target string (second argument, target)
+function confirmEnding(str, target) {
+  return str.slice(str.length - target.length) === target;
+}
+confirmEnding("Bastian", "n");
