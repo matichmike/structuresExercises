62281910ae85a9d204cd9e814dc6f7a1818bf560
Michael Matich <matich.mike@gmail.com>
2020-08-11 11:26:09 -0400

    sliding window exercises added

--- algorithmScripting.js
+++ algorithmScripting.js
@@ -273,3 +273,38 @@ function countUniqueValues(array){
     }
     return i + 1;
   }
+
+// sliding window pattern - maxSubarraySum - calculate the max sum of n consecutive elements in array
+// O(n^2)
+function maxSubarraySum(arr, num) {
+  if (num > arr.length) {
+    return null;
+  }
+  const max = -Infinity;
+  for (let i = 0; i < arr.length - num + 1; i++) {
+    temp = 0;
+    for (let j = 0; j < num; j++) {
+      temp += arr[i+j];
+    }
+    if (temp > max) {
+      max = temp;
+    }
+  }
+  return max;
+}  
+
+// sliding window O(n) linear complexity solution
+function maxSubarraySum(arr, num) {
+  let maxSum = 0;
+  let tempSum = 0;
+  if (arr.length < num) return null;
+  for (let i = 0; i < num; i++) {
+    maxSum += arr[i]
+  }
+  tempSum = maxSum;
+  for (let i = num; i < arr.length; i++) {
+    tempSum = tempSum - arr[i - num] + arr[i];
+    maxSum = Math.max(maxSum, tempSum);
+  }
+  return maxSum;
+}
