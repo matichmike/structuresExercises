109fce70729f36d6882e3743ac80c8f3d3781caf
Michael Matich <matich.mike@gmail.com>
2020-08-11 12:10:55 -0400

    binary search exercise

--- algorithmScripting.js
+++ algorithmScripting.js
@@ -308,3 +308,22 @@ function maxSubarraySum(arr, num) {
   }
   return maxSum;
 }
+
+// binary search - find the index of given element in the array - O(log n)
+function search(array, val) {
+  let min = 0;
+  let max = array.length - 1;
+
+  while (min <= max) {
+    let middle = Math.floor((min + max) / 2);
+    let currentElement = array[middle];
+    if(array[middle] < val) {
+      min = middle + 1;
+    } else if (array[middle] > val) {
+      max = middle - 1;
+    } else {
+      return middle;
+    }
+  }
+  return -1;
+}
