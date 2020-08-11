c432737f629b84bbe33fffadaadbcfb1e9d5b6c4
Michael Matich <matich.mike@gmail.com>
2020-08-10 23:05:57 -0400

    frequency counter exercises added

--- algorithmScripting.js
+++ algorithmScripting.js
@@ -156,3 +156,65 @@ function charCount(str) {
 }
 
 console.log(charCount("Hello hey"))
+
+// check if second array contains the squared elements of the first one
+// O(n^2) solution
+function same(arr1, arr2) {
+  if(arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    let correctIndex = arr2.indexOf(arr1[i]** 2)
+    if(correctIndex === -1) {
+      return false;
+    }
+    arr2.splice(correctIndex, 1);
+  }
+  return true;
+}
+
+// O(n) solution
+function same(arr1, arr2) {
+  if(arr1.length !== arr2.length) {
+    return false;
+  }
+  let frequencyCounter1 = {};
+  let frequencyCounter2 = {};
+  for (let val of arr1) {
+    frequencyCounter1[val] = (frequencyCounter1[val] || 0) + 1;
+  }
+  for (let val of arr2) {
+    frequencyCounter2[val] = (frequencyCounter2[val] || 0) + 1;
+  }
+  for (let key in frequencyCounter1) {
+    if(!(key ** 2 in frequencyCounter2)) {
+      return false;
+    }
+    if (frequencyCounter2[key ** 2] !== frequencyCounter1[key]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+// check if two given strings are anagrams
+function validAnagram(first, second) {
+  if (first.length !== second.length) {
+    return false;
+  }
+  const lookup = {};
+  for (let i = 0; i < first.length; i++) {
+    let letter = first[i];
+    lookup[letter] ? lookup[letter] += 1 : lookup[letter] = 1;
+  }
+  for (let i = 0; i < second.length; i++) {
+    let letter = second[i];
+    if (!lookup[letter]) {
+      return false;
+    } else {
+      lookup[letter] -= 1;
+    }
+  }
+  return true;
+}
+console.log('anagram is', validAnagram('anagram', 'nagaram'))
