a3772bf840bf4dfe761c3c4a0be6be78cea4193b
Michael Matich <matich.mike@gmail.com>
2020-08-12 15:54:34 -0400

    quick sort example

--- sorting.js
+++ sorting.js
@@ -96,3 +96,27 @@ function mergeSort(arr) {
   let right = mergeSort(arr.slice(mid));
   return merge(left, right);
 }
+
+// quick sort
+// pivot helper
+function pivot(arr, start = 0, end = arr.length + 1) {
+  let pivot = arr[start];
+  let swapIdx = start;
+  for(let i = start + 1; i < arr.length; i++) {
+    if(pivot > arr[i]) {
+      swapIdx++;
+      swap(arr, swapIdx, i)
+    }
+  }
+  swap(arr, start, swapIdx);
+  return swapIdx;
+}
+
+function quickSort(arr, left = 0, right = arr.length - 1) {
+  if(left < right) {
+  let pivotIndex = pivot(arr, left, right);
+  quickSort(arr, left, pivotIndex - 1);
+  quickSort(arr, pivotIndex + 1, right);
+  }
+  return arr;
+}
