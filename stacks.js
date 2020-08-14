7f6a8f9522fdd27ab160d6f995388055a641e928
Michael Matich <matich.mike@gmail.com>
2020-08-13 23:07:35 -0400

    stack and queue examples

---
+++ stacks.js
@@ -0,0 +1,78 @@
+// singly linked list stack
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+
+  push(val) {
+    let newNode = new Node(val);
+    if(!this.first) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      let temp = this.first;
+      this.first = newNode;
+      this.first.next = temp;
+    }
+    return this.size++;
+  }
+
+  pop() {
+    if(!this.first) return null;
+    let temp = this.first;
+    if(this.first === this.last) {
+      this.last = null;
+    }
+    this.first = this.first.next;
+    this.size--;
+    return temp.value;
+  }
+}
+
+// singly linked list queue stack
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Queue {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+  //add to the end
+  endqueue(val) {
+    let newNode = new Node(val);
+    if(!this.first) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      this.last.next = newNode;
+      this.last = newNode;
+    }
+    return this.size++;
+  }
+  //remove from beginning
+  dequeue() {
+    if(!this.first) return null;
+    let temp = this.first;
+    if(this.first === this.last) {
+      this.last = null;
+    }
+    this.first = this.first.next;
+    this.size--;
+    return temp.value;
+  }
+}
