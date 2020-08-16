ff3f915ea267a56ba570f136e121b42d1532484d
Michael Matich <matich.mike@gmail.com>
2020-08-16 00:04:39 -0400

    dfs recursive search

--- graphs.js
+++ graphs.js
@@ -24,4 +24,21 @@ class Graph {
     }
     delete this.adjacencyList[vertex];
   }
+
+  depthFirstRecursive(start) {
+    const result = [];
+    const visited = {};
+    const adjacencyList = this.adjacencyList
+    (function dfs(vertex) {
+      if(!vertex) return null;
+      visited[vertex] = true;
+      result.push(vertex);
+      adjacencyList[vertex].forEach(neighbor => {
+        if(!visited[neighbor]) {
+          return dfs(neighbor);
+        }
+      })
+    }) (start);
+    return result;
+  }
 }
