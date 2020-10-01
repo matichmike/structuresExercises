2c1dfdf6355a344ceec1101baf6db35caa99c9d0
Michael Matich <matich.mike@gmail.com>
2020-10-01 17:38:59 -0400

    prime number check

--- algorithmScripting.js
+++ algorithmScripting.js
@@ -364,3 +364,18 @@ switch (argument) {
   default:
     alert('Undefined');
 }
+
+// prime number check
+function showPrime(n) {
+  for (let i = 2; i < n; i++) {
+    if (!isPrime(i)) continue;
+    alert(i);
+  }
+}
+
+function isPrime(n) {
+  for (let i = 2; i < n; i++) {
+    if (n % i == 0) return false;
+  }
+  return true;
+}
