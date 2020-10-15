7913e24a7008086d252e0431ecb7f781840617b6
Michael Matich <matich.mike@gmail.com>
2020-10-15 08:59:57 -0400

    ladder object example

--- algorithmScripting.js
+++ algorithmScripting.js
@@ -416,10 +416,29 @@ let calc = {
   },
 
   read() {
-    this.a = +prompt('a?', 0);
-    this.b = +prompt('b', 0);
+    this.a = prompt("a?", 0);
+    this.b = prompt("b?", 0);
   }
 };
+
 calc.read();
 alert(calc.sum());
 alert(calc.mul());
+
+// ladder w/ chaining
+let ladder = {
+  step: 0,
+  up() {
+    this.step++;
+    return this;
+  },
+  down() {
+    this.step--;
+    return this;
+  },
+  showStep: function() {
+    alert(this.step);
+    return this;
+  }
+};
+ladder.up().up().down().showStep();
