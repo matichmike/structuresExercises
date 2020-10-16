b3ea2e7bb8bc911ab9faae84b7d6992d4c29911c
Michael Matich <matich.mike@gmail.com>
2020-10-16 09:10:39 -0400

    new obj calc

--- algorithmScripting.js
+++ algorithmScripting.js
@@ -442,3 +442,25 @@ let ladder = {
   }
 };
 ladder.up().up().down().showStep();
+
+// new obj calc
+function Calculator() {
+
+  this.read = function() {
+    this.a = prompt("a?", 0);
+    this.b = prompt("b", 0);
+  }
+
+  this.sum = function() {
+    return this.a + this.b;
+  }
+
+  this.mul = function() {
+    return this.a * this.b;
+  }
+
+}
+let calculator = new Calculator;
+calculator.read();
+alert("Sum=" + calculator.sum());
+alert("Mul=" + calculator.mul());
