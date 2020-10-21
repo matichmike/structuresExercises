07ca1887330aad9d7a218dbeabb2510fe4f8db52
Michael Matich <matich.mike@gmail.com>
2020-10-20 22:55:59 -0400

    max arr sum

--- arrays.js
+++ arrays.js
@@ -104,3 +104,16 @@ function sumInput() {
   }
   return sum;
 }
+
+// max continious sum of arr elems O(n^2)
+function getMaxSubSum(arr) {
+  let maxSum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    let sumFixedStart = 0;
+    for (let j = i; j < arr.length; j++) {
+      sumFixedStart += arr[j];
+      maxSum = Math.max(maxSum, sumFixedStart);
+    }
+  }
+  return maxSum;
+}
