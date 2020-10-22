2d6e58e132ad74ffa6df6b485a748c5d9f09ba34
Michael Matich <matich.mike@gmail.com>
2020-10-22 11:29:04 -0400

    age sort

--- arrays.js
+++ arrays.js
@@ -200,3 +200,14 @@ let usersMapped = users.map(user => ({
   fullName: `${user.name} ${user.surname}`,
   id: user.id
 }));
+
+// sort by age
+let vasya = { name: "Вася", age: 25 };
+let petya = { name: "Петя", age: 30 };
+let masha = { name: "Маша", age: 28 };
+
+let arr = [ vasya, petya, masha ];
+
+function sortByAge(arr) {
+  arr.sort((a,b) => a.age > b.age ? 1 : -1)
+};
