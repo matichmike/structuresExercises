7dcc6ae18f23adc6a33b35e74a8fb39041fe2f74
Michael Matich <matich.mike@gmail.com>
2020-10-22 10:43:10 -0400

    camelize string

--- arrays.js
+++ arrays.js
@@ -129,3 +129,12 @@ function getMaxSubSum(arr) {
   }
   return maxSum;
 }
+
+// camelize fn
+function camelize(str) {
+  return str
+    .split('-')
+    .map((word, index) => index == 0 ? word : word[0].toUpperCase() + word.slice(1)
+    )
+    .join('');
+}
