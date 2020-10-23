f4a0e40f9ba21e419985983cdde8d6725542b36c
Michael Matich <matich.mike@gmail.com>
2020-10-22 21:58:57 -0400

    anagram filter

--- mapAndSet.js
+++ mapAndSet.js
@@ -3,3 +3,15 @@ function unique(arr) {
   return Array.from(new Set(arr));
 }
 
+// anagram filter
+function aclean(arr) {
+  let map = new Map();
+  for (let word of arr) {
+    let sorted = word.toLowerCase().split('').sort().join('');
+    map.set(sorted, word);
+  }
+  return Array.from(map.values);
+}
+
+let arr = ["nap", "teachers", "cheaters", "PAN", "ear", "era", "hectares"];
+console.log(aclean(arr));
