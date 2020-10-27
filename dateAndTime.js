6abbe5ce3bc4624206dd654a2d979724033b7bbb
Michael Matich <matich.mike@gmail.com>
2020-10-27 11:00:55 -0400

    format date

--- dateAndTime.js
+++ dateAndTime.js
@@ -38,3 +38,22 @@ function getSecondsToTomorrow() {
   let diff = tomorrow - now;
   return Math.round(diff / 1000);
 }
+
+//formatDate
+function formatDate(date) {
+  let diff = new Date() - date;
+  if(diff < 1000) return 'just now';
+  let sec = Math.floor(diff/1000);
+  if(sec < 60) return sec + ' seconds ago';
+  let min = Math.floor(diff/60000);
+  if(min < 60) return min + ' minutes ago';
+  let d = date;
+  d = [
+    '0' + getDate(),
+    '0' + (d.getMonth() + 1),
+    '' + d.getFullYear(),
+    '0' + d.getHours(),
+    '0' + d.getMinutes()
+  ].map(component => component.slice(-2));
+  return d.slice(0,3).join('.') + ' ' + d.slice(3).join(':');
+}
