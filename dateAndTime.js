a4c840323fefe2557c060f736a37702dfa2e30bd
Michael Matich <matich.mike@gmail.com>
2020-10-27 10:39:27 -0400

    getDateAgo

--- dateAndTime.js
+++ dateAndTime.js
@@ -6,3 +6,12 @@ function getWeekDay(date) {
   let days = ['su', 'mo', 'tu', 'we', 'th', 'fr', 'sa'];
   return days[date.getDay()]
 }
+
+//getDateAgo
+function getDateAgo(date, days) {
+  let dateCopy = new Date(date);
+  dateCopy.setDate(date.getDate() - days);
+  return dateCopy.getDate();
+}
+let date = new Date(2015, 0, 2);
+console.log(getDateAgo(date, 365));
