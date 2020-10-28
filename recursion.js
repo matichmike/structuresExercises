f234ed5414a0484868fb40117b01b7a8dbe950ba
Michael Matich <matich.mike@gmail.com>
2020-10-28 11:44:31 -0400

    printing the nested obj elements recursively

--- recursion.js
+++ recursion.js
@@ -142,3 +142,54 @@ function fib(n) {
   }
   return b;
 }
+
+// print elements one by one
+let list = {
+  value: 1,
+  next: {
+    value: 2,
+    next: {
+      value: 3,
+      next: {
+        value: 4,
+        next: null
+      }
+    }
+  }
+};
+//loop
+function printList(list) {
+  let tmp = list;
+  while(tmp) {
+    alert(tmp.value);
+    tmp = tmp.next;
+  }
+}
+//recursion
+function printList(list) {
+  alert(list.value);
+  if (list.next) {
+    printList(list.next);
+  }
+}
+
+//same but in reverse
+//loop
+function printReverseList(list) {
+  let arr = [];
+  let tmp = list;
+  while (tmp) {
+    arr.push(tmp.value);
+    tmp = tmp.next;
+  }
+  for (let i = arr.length - 1; i >= 0; i--) {
+    alert(arr[i])
+  }
+}
+//recursion
+function printReverseList(list) {
+  if(list.next) {
+    printReverseList(list.next);
+  }
+  alert(list.value);
+}
