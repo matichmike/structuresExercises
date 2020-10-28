97e0ca65d48db352c9dd3c39eb7f739e1d9806b0
Michael Matich <matich.mike@gmail.com>
2020-10-28 09:36:02 -0400

    eliminate looping links

--- JSON.js
+++ JSON.js
@@ -11,4 +11,35 @@ room.occupiedBy = meetup;
 
 console.log(JSON.stringify(meetup, function replacer(key, value) {
   return (key == 'occupiedBy') ? undefined : value;
-}));
+}, 2));
+
+// parse with new date
+let schedule = `{
+  "meetups": [
+    {"title":"Conference","date":"2017-11-30T12:00:00.000Z"},
+    {"title":"Birthday","date":"2017-04-18T12:00:00.000Z"}
+  ]
+}`;
+schedule = JSON.parse(schedule, function(key, value) {
+  if (key == 'date') return new Date(value);
+  return value;
+});
+console.log(schedule.meetups[1].date.getDate());
+
+// eliminate the loopin links
+let room = {
+  number: 23
+};
+
+let meetup = {
+  title: "Huddle",
+  occupiedBy: [{name: "Ivanov"}, {name: "Petrov"}],
+  place: room
+};
+
+room.occupiedBy = meetup;
+meetup.self = meetup;
+
+alert(JSON.stringify(meetup, function replacer(key, value) {
+  return (key != "" && value == meetup) ? undefined : value;
+}))
