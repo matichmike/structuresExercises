b55484bac333a4e6c7110ac663f950adc3b72aee
Michael Matich <matich.mike@gmail.com>
2020-10-28 11:22:24 -0400

    sumto fn

--- recursion.js
+++ recursion.js
@@ -93,3 +93,25 @@ function sumSalaries(department) {
     return sum;
   }
 }
+
+//sumTo function
+function sumTo(n) {
+  if (n == 1) {
+    return 1
+  } else {
+    return n + sumTo(n - 1);
+  }
+}
+
+function sumTo(n) {
+  let sum = 0;
+  for (let i = n; i > 0; i--) {
+    sum += i;
+  }
+  return sum;
+}
+
+function sumTo(n) {
+  return n * (n + 1) / 2;
+}
+console.log(sumTo(100))
