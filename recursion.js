6be79d75e4bd14e121d79406e5f4d9f9e8434f68
Michael Matich <matich.mike@gmail.com>
2020-10-28 11:34:10 -0400

    fibonacci

--- recursion.js
+++ recursion.js
@@ -125,3 +125,20 @@ function factorial(n) {
   }
 }
 console.log(factorial(5))
+
+//fibonacci sequence
+// recursion
+function fib(n) {
+  return n <= 1 ? n : fib(n - 1) + fib(n - 2);
+}
+// faster
+function fib(n) {
+  let a = 1;
+  let b = 1;
+  for (let i = 3; i <= n; i++) {
+    let c = a + b;
+    a = b;
+    b = c;
+  }
+  return b;
+}
