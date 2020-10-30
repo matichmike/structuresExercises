16a85f422e0a38337db2afdfecdea4c4c405abbf
Michael Matich <matich.mike@gmail.com>
2020-10-30 11:38:10 -0400

    obj sort

--- closures.js
+++ closures.js
@@ -24,3 +24,15 @@ function inArray(arr) {
 
 let arr = [1,2,3,4,5,6,7];
 alert(arr.filter(inArray([1,2,10])));
+
+// sort by value
+let users = [
+  { name: "John", age: 20, surname: "Johnson" },
+  { name: "Pete", age: 18, surname: "Peterson" },
+  { name: "Ann", age: 19, surname: "Hathaway" }
+];
+function byField(field) {
+  return (a,b) => a[field] > b[field] ? 1 : -1;
+}
+users.sort(byField('name'));
+users.forEach(user => alert(user.name))
