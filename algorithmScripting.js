e68ede1b756ff817e7dec8db8bf1c88e50310407
Michael Matich <matich.mike@gmail.com>
2020-11-02 14:41:42 -0500

    makeCounter

--- algorithmScripting.js
+++ algorithmScripting.js
@@ -549,3 +549,14 @@ let sayHi = function func(who) {
 let welcome = sayHi;
 sayHi = null;
 welcome();
+
+//makeCounter
+function makeCounter() {
+  let count = 0;
+  function counter() {
+    return count++;
+  }
+  counter.set = value => count = value;
+  counter.decrease = () => count--;
+  return counter;
+}
