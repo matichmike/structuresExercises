eb3965a2a966cf4aa962404c3c27e3512b804210
Michael Matich <matich.mike@gmail.com>
2020-11-03 09:54:00 -0500

    settimeout recursive

--- algorithmScripting.js
+++ algorithmScripting.js
@@ -573,3 +573,17 @@ function printNumbers(from, to) {
   }, 1000)
 }
 printNumbers(5, 10);
+
+//setInterval recursive
+function printNumbers(from, to) {
+  let current = from;
+
+  setTimeout(function go() {
+    alert(current);
+    if(current < to) {
+      setTimeout(go, 1000);
+    }
+    current++;
+  }, 1000);
+}
+printNumbers(5, 10);
