17468f3fa2248f742c12522baab73441659c1bbb
Michael Matich <matich.mike@gmail.com>
2020-11-26 23:25:49 -0500

    async await example

--- algorithmScripting.js
+++ algorithmScripting.js
@@ -587,3 +587,15 @@ function printNumbers(from, to) {
   }, 1000);
 }
 printNumbers(5, 10);
+
+// async await example
+async function loadJson(url) {
+  let response = await fetch(url);
+  if (response.status == 200) {
+    let json = await response.json();
+    return json;
+  }
+  throw new Error(response.status);
+}
+loadJson('no such user json')
+  .then(alert);
